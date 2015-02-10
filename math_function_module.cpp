95a5fd38302fa59b13977738b7adeaa4fe1ff504
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2015-02-10 13:12:04 +0500

    Update math_function_module.cpp
    Добавил описание executeFunction.
    Убрал тестовые классы и структуры.
    main оставил для проверки.

--- math_function_module.cpp
+++ math_function_module.cpp
@@ -1,62 +1,93 @@
 
+#include <stddef.h> // Чтобы определить NULL
+
+///////// Включения основного модуля module.h
+#include "Module.h"
 #include "Function_module.h"
 
+#include <math.h>
+#include <iostream>
+//#include <time.h> // для Рандомайзера 
+
+
 using namespace std;
-
-///////////////// Тестовые классы и структуры
-struct FS {
-	int first;
-	int second;
-};
-    
-class FuncModule {
-protected:
-	virtual FS** getFunc() = 0;
-};
-
-class FuncRealized : public FuncModule{
-
-public:
-	FS InnerClassFS; //Объявлена структура типа FS
-	FS **robot_functions;
-	FS** getFunc(){ //функция/метод должна вернуть указатель на указатель на структуру типа ФС
-		cout << "getFunc WORKS!" << endl;
-
-		InnerClassFS.first = 11;
-		InnerClassFS.second = 20;
-
-		FS *pTestFS;    // Указатель на структуру типа FS
-		static FS **ppTestFS;  // Указатель на указатель на структуру типа FS
-		pTestFS = &InnerClassFS; //присваиваем адрес структуры описанной в классе
-		ppTestFS = &pTestFS;  //присваеваем адрес адреса структуры опис. в классе
-		// Проверка значений указателей
-		cout << pTestFS << " Указатель на структуру " << endl;
-		cout << ppTestFS << " Указатель на указатель на структуру " << endl;
-		robot_functions = new FS*[1];
-		return robot_functions;
-	};
-	FuncRealized() {
-		//robot_functions = new FS*[COUNT_MATH_FUNCTIONS];
-		int function_id = 0;
-	};
-};
-/////////////////// конец тестовых классов и структур
-
-
-
 
 
 // Опишу функцию вне класса чтобы можно было применять cout
 FunctionResult* MathFunctionModule::executeFunction(regval functionId, regval *args){
 	cout << "Func executed" << endl;
-	if (functionId == 1) {
-		cout << "a+b = a+b" << endl;
-	} //endIf
-	//char ch1 = '1';
-	char ch4 = '1';
-	//FunctionResult  *rez2;
-	//char ch4='1';
+	if (!functionId) {
+		return NULL;
+	}
+
+	// эта конструкция чтобы создать заранее объект указатель на который будем возвращать
+	char ch4 = '1'; // имя надо будет поменять  или просто что-то поменять. Но пока символ оставим в покое и буем работать с result.
 	rez = new FunctionResult(ch4);
+
+	// Уже не тестовое задание пробуем описать
+	switch (functionId) {
+		//первая функция pow - квадрат. 1-й аргумент возводимое, второй - степень.
+		case 1:  // Тут вопрос про приведение типов
+		{
+			/*C приведением типов
+			double arg1,arg2,resOfPow;
+			arg1=*args;
+			arg2=*(args+1);
+			resOfPow=pow(arg1,arg2);
+			rez->result = (int) resOfPow;
+			*/
+	
+			// Пока сделаем тупо так, чтобы не приводить типы
+			int sum = 1;
+			int tempArg1 = (*args); // на всякий слуяай чтобы точно прочиталось что мы будем ее умножать
+			for (int i = 1; i <= *(args + 1); i++) {
+				sum = sum * tempArg1;
+			};
+			//cout << args << " " << *args << " " << *(args + 1) << " " << sum << endl;
+			rez->result = sum;
+			// Все сделали и выходим из свича
+			break;
+		}
+		case 2: // ABS модуль числа
+		{
+		/*C приведением типов*/ 
+			int resOfabs;
+			resOfabs = abs(*args);
+			rez->result = resOfabs;
+			// Все сделали и выходим из свича
+			break;
+		}
+		case 3: // FMOD Остаток от деления
+		{
+			int resOfmod;
+			resOfmod = (int) fmod(*args,*(args +1));
+			rez->result = resOfmod;
+			// Все сделали и выходим из свича
+			break;
+		}
+		case 4: // DIV Целочисленное деление
+			int resOfdiv;
+			resOfdiv = ( *args - (int)fmod(*args, *(args + 1)) ) / (*(args+1));
+			rez->result = resOfdiv;
+			// Все сделали и выходим из свича
+			break;
+	
+		case 5: // Установка Рандомайзера
+		{
+			//srand(time(NULL));
+			rez->result = 1;
+			// Все сделали и выходим из свича
+			break;
+		}
+		case 6:
+		{
+			int resOfrand;
+			resOfrand = rand()%(*args) + (*(args+1)) ;
+			rez->result = resOfrand;
+			// Все сделали и выходим из свича
+			break;
+		}
+    }; // Конец switch
 	return rez;
 };
 
@@ -65,40 +96,67 @@ FunctionResult* MathFunctionModule::executeFunction(regval functionId, regval *a
 
 
 int main(){
+	int test;
+	test = 1;
+	cout << test << endl;
 
-	FS **pointFStruct; // Объявили указатель на указатель на структуру типа FS
-	FS *pFSStruct;
+
 	regval *arg1;
 	regval x;
 	x = 1;
 	arg1 = &x;
 
-	FuncRealized FRealObject; // Объявили объект
-
-	pointFStruct = FRealObject.getFunc();  // Сработала функция/метод
-	pFSStruct = (*(pointFStruct));
-	cout << pointFStruct << " _ _ _ " << pFSStruct << " _ _ _ " <<*(pointFStruct) << endl; // Выведем Указатель на указатель и Указатель
-	cout << pointFStruct << " _ _ _ " << pFSStruct << " _ _ _ " << *(pointFStruct) << endl; // Выведем Указатель на указатель и Указатель
-	cout << pointFStruct << " _ _ _ " << pFSStruct << " _ _ _ " << *(pointFStruct) << endl; // Выведем Указатель на указатель и Указатель
-	cout << pointFStruct << " _ _ _ " << *(pointFStruct) << endl; // Выведем Указатель на указатель и Указатель
-	cout << pointFStruct << " _ _ _ " << *(pointFStruct) << endl; // Выведем Указатель на указатель и Указатель
-
-	cout << (*(pointFStruct)) << endl;
-	cout << &(FRealObject.InnerClassFS.first) << endl;
-
+	regval TwoArgs[] = {2,3};
+	regval absTestMas[] = { -14 };
+	regval fmodTestMas[] = { 7, 4 };
+	regval divTestMas[] = { 9, 4 };
+	regval randTestMas[] = { 10, 1 };
+	
+	
 	MathFunctionModule newObject;
 
-	char ch3 = '1';
 	FunctionResult *FRobject;
 
-	cout << newObject.testInt << endl;
-	newObject.testInt = 1;
-	cout << newObject.testInt << endl;
+	// проверка возведения в степень
+	FRobject = newObject.executeFunction(1, TwoArgs); // не забываем что этот объект ссылается на свойство newObject. 
+	cout << FRobject << endl;
+	cout << FRobject->type << endl;
+	cout << FRobject->result << endl;
 
-	FRobject = newObject.executeFunction(1,arg1);
+   
+
+	// Проверка получения абсолютной величины
+	FRobject = newObject.executeFunction(2, absTestMas); // не забываем что этот объект ссылается на свойство newObject. 
 	cout << FRobject << endl;
+	cout << FRobject->type << endl;
+	cout << FRobject->result << endl;
+
+	// Проверка остатка от деления
+	FRobject = newObject.executeFunction(3, fmodTestMas); // не забываем что этот объект ссылается на свойство newObject. 
 	cout << FRobject << endl;
+	cout << FRobject->type << endl;
+	cout << FRobject->result << endl;
+
+	// Проверка целочисленного деления на том же массиве
+	FRobject = newObject.executeFunction(4, divTestMas); // не забываем что этот объект ссылается на свойство newObject. 
 	cout << FRobject << endl;
+	cout << FRobject->type << endl;
+	cout << FRobject->result << endl;
+	
+	cout << "- - - " << newObject.executeFunction(4, divTestMas)->result + newObject.executeFunction(4, divTestMas)->result << endl;
+
+	// Проверка Установка рандомайзера. не важно что передаем в аргументах
+	//FRobject = newObject.executeFunction(5, divTestMas); // не забываем что этот объект ссылается на свойство newObject. 
+	//cout << FRobject << endl;
+	//cout << FRobject->type << endl;
+	//cout << FRobject->result << endl;
+
+	// Проверяем рандомайзер 
+	FRobject = newObject.executeFunction(6, randTestMas); // не забываем что этот объект ссылается на свойство newObject. 
+	cout << FRobject->result << endl;
+	FRobject = newObject.executeFunction(6, randTestMas); // не забываем что этот объект ссылается на свойство newObject. 
+	cout << FRobject->result << endl;
+	
 
 	return 0;
 }
