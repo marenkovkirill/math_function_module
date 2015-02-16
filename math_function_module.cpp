4480fb601f1039c92fb543c900da289c9842c537
unknown <X:\The Bat\Data\Mail>
2015-02-16 20:56:03 +0500

    no message

--- math_function_module.cpp
+++ math_function_module.cpp
@@ -1,227 +1,163 @@
 #include <stddef.h> // Чтобы определить NULL
 #include <time.h> // для Рандомайзера 
 
-///////// Включения основного модуля module.h
-#include "Module.h"
-#include "Function_Module.h"
-#include "Math_Function_Module.h"
+#include "../module_headers/module.h"
+#include "../module_headers/function_Module.h"
+#include "math_function_module.h"
 
 #include <math.h>
 #include <iostream>
 
-//using namespace std;
-
 ////////////// Установка Глобальных переменных.
-// GLOBAL VARIABLES
 int COUNT_MATH_FUNCTIONS = 15;
 
-// Опишем макросс который будет заполнять наш массив структур с информацией о функциях. Ия функции, Присваивает уникальный индекс, число параметров и дает ли исключение.
+// Опишем макрос который будет заполнять наш массив структур с информацией о функциях. Ия функции, Присваивает уникальный индекс, число параметров и дает ли исключение.
 #define ADD_MATH_FUNCTION(FUNCTION_NAME, COUNT_PARAMS, GIVE_EXCEPTION) \
-math_functions[function_id] = new FunctionData; \
-math_functions[function_id]->command_index = function_id + 1; \
-math_functions[function_id]->count_params = COUNT_PARAMS; \
-math_functions[function_id]->give_exception = GIVE_EXCEPTION; \
-math_functions[function_id]->name = FUNCTION_NAME; \
-function_id++;
-// важная часть чтобы был уникальный параметр 
-// начинается от нуля поэтому такой вид. В конструкторе перед использованием function_id будет присвоен ноль.
-/////////////////////////////////////////////////////////////////////////////////
+	math_functions[function_id] = new FunctionData; \
+	math_functions[function_id]->command_index = function_id + 1; \
+	math_functions[function_id]->count_params = COUNT_PARAMS; \
+	math_functions[function_id]->give_exception = GIVE_EXCEPTION; \
+	math_functions[function_id]->name = FUNCTION_NAME; \
+	function_id++;
 
 // Опишем макросс который все наши функции заполнит/ Добавил Функцию - Увеличивай их число COUNT_MATH_FUNCTIONS. А то удалишь нафиг какой-нить процесс в памяти.
 #define DEFINE_ALL_FUNCTIONS \
-	ADD_MATH_FUNCTION("pow",  2,false) \
-	ADD_MATH_FUNCTION("abs",  1,false) \
-	ADD_MATH_FUNCTION("mod",  2,false) \
-	ADD_MATH_FUNCTION("div",  2,false) \
-	ADD_MATH_FUNCTION("sqrt", 1,true) \
-	ADD_MATH_FUNCTION("rand", 2,true) \
-	ADD_MATH_FUNCTION("sin",  1,false) \
-	ADD_MATH_FUNCTION("cos",  1,false) \
-	ADD_MATH_FUNCTION("tan",  1,false) \
-	ADD_MATH_FUNCTION("asin", 1,true) \
-	ADD_MATH_FUNCTION("acos", 1,true) \
-	ADD_MATH_FUNCTION("atan", 1,false) \
-	ADD_MATH_FUNCTION("exp",  1,false) \
-	ADD_MATH_FUNCTION("log",  1,true) \
-	ADD_MATH_FUNCTION("log10",1,true) 
-// Конец макроса
+	ADD_MATH_FUNCTION("pow",  2, false) \
+	ADD_MATH_FUNCTION("abs",  1, false) \
+	ADD_MATH_FUNCTION("mod",  2, false) \
+	ADD_MATH_FUNCTION("div",  2, false) \
+	ADD_MATH_FUNCTION("sqrt", 1, true) \
+	ADD_MATH_FUNCTION("rand", 2, true) \
+	ADD_MATH_FUNCTION("sin",  1, false) \
+	ADD_MATH_FUNCTION("cos",  1, false) \
+	ADD_MATH_FUNCTION("tan",  1, false) \
+	ADD_MATH_FUNCTION("asin", 1, true) \
+	ADD_MATH_FUNCTION("acos", 1, true) \
+	ADD_MATH_FUNCTION("atan", 1, false) \
+	ADD_MATH_FUNCTION("exp",  1, false) \
+	ADD_MATH_FUNCTION("log",  1, true) \
+	ADD_MATH_FUNCTION("log10",1, true)
 
-// Далее описание методов класса МathFunctionModule
-// Метод UID 
 const char* MathFunctionModule::getUID() {
-	return "Math_Functions_dll";
+    return "Math_Functions_dll";
 };
 
-// метод получения данных о наших функциях
-FunctionData**  MathFunctionModule::getFunctions(int *count_functions){ // Эта функция даст указатель на массив струкктур где описаны функции
-	*count_functions = COUNT_MATH_FUNCTIONS; // Присваиваем в переменную(параметр функции) число наших функций
-	return math_functions;
+FunctionData**  MathFunctionModule::getFunctions(int *count_functions) {
+    *count_functions = COUNT_MATH_FUNCTIONS;
+    return math_functions;
 };
 
-// Конструктор класса
-MathFunctionModule::MathFunctionModule() {// В этом конструкторе сразу создадим массив структур
-	math_functions = new FunctionData*[COUNT_MATH_FUNCTIONS];// Использует глобальную переменную. Это выделение памяти под массив указателей на структуры FunctionData из MSDN
-	regval function_id = 0; // С этим идентефикатором работает DEFINE_ALL_FUNCTIONS
-	srand(time(NULL)); // зададим сразу рандомайзхер - вдруг пригодится
-	DEFINE_ALL_FUNCTIONS
+MathFunctionModule::MathFunctionModule() {
+    srand(time(NULL));
+	
+	math_functions = new FunctionData*[COUNT_MATH_FUNCTIONS];
+    regval function_id = 0;
+    DEFINE_ALL_FUNCTIONS
 };
 
-// Деструктор напишем отдельным методом. Он удалит структуры с нашими функциями и потом сам оъект.
 void MathFunctionModule::destroy() {
-	for (int j = 0; j < COUNT_MATH_FUNCTIONS; ++j) {
-		delete math_functions[j];
-	}
-	delete[] math_functions;
-	delete this;
+    for (int j = 0; j < COUNT_MATH_FUNCTIONS; ++j) {
+        delete math_functions[j];
+    }
+    delete[] math_functions;
+    delete this;
 };
 
-// Метод - выполнить математическую функцию
-FunctionResult* MathFunctionModule::executeFunction(regval functionId, regval *args){
-	if ( (functionId<1) || (functionId >15) ) {
-		return NULL;
-	}
+FunctionResult* MathFunctionModule::executeFunction(regval functionId, regval *args) {
+    if ((functionId < 1) || (functionId > 15)) {
+        return NULL;
+    }
 
-	// эта конструкция чтобы создать заранее объект указатель на который будем возвращать
-	char ch4 = '1'; // имя надо будет поменять  или просто что-то поменять. Но пока символ оставим в покое и буем работать с result.
-	rez = new FunctionResult(1);
-	// Try Catch БЛОК
-	try {
-		// Уже не тестовое задание пробуем описать
-		switch (functionId) {
-			//первая функция pow - квадрат. 1-й аргумент возводимое, второй - степень.
-		case 1:
-		{
+    FunctionResult *rez = new FunctionResult(1);
+	bool throw_exception = false;
+
+    switch (functionId) {
+		case 1: {
 			rez->result = (int)pow((double)*args, (double)(*(args + 1)));
-			// Все сделали и выходим из свича
 			break;
 		}
-		case 2: // ABS модуль числа
-		{
+		case 2: {
 			rez->result = abs(*args);
-			// Все сделали и выходим из свича
 			break;
 		}
-		case 3: // FMOD Остаток от деления
-		{
+		case 3: {
 			rez->result = (int)fmod(*args, *(args + 1));
-			// Все сделали и выходим из свича
 			break;
 		}
-		case 4: // DIV Целочисленное деление
+		case 4: {
 			rez->result = (*args - (int)fmod(*args, *(args + 1))) / (*(args + 1));
-			// Все сделали и выходим из свича
-			break;
-
-		case 5: // Корень 2-й степени Результат будем выводить умноженным на 1000, потому что растет медленно(меняется медленно)
-		{
-			if (*args>=0) {
-				rez->result = (int)(1000 * sqrt(*args));
-			}
-			else {
-				throw;
-			} // Конец if
 			break;
 		}
-		case 6: // Рандомное целое число
-		{
-			//rez->result = rand() % (*args) + (*(args + 1));
-			if (*args > 0) {
-				rez->result = rand() % (*args) + (*(args + 1));
+		case 5: {
+			if (*args < 0) {
+				throw_exception = true;
 			}
-			else {
-				throw;
-			} // Конец if
-			// Все сделали и выходим из свича
+			rez->result = (int)(1000 * sqrt(*args));
 			break;
 		}
-		case 7: // Синус Изменяется быстро поэтому ввод просим умножить на 1000 а в вычислениях введенное будем делить на 1000 и выводить будем умножив на 1000, потому что быстро меняется в малых пределах
-		{
+		case 6: {
+			if (*args <= 0) {
+				throw_exception = true;
+			}
+			rez->result = rand() % (*args) + (*(args + 1));
+			break;
+		}
+		case 7: {
 			rez->result = (int)(1000 * sin(((double)*args) / 1000));
-			// Все сделали и выходим из свича
 			break;
 		}
-		case 8: // Косинус Изменяется быстро поэтому ввод просим умножить на 1000 а в вычислениях введенное будем делить на 1000 и выводить будем умножив на 1000, потому что быстро меняется в малых пределах
-		{
+		case 8: {
 			rez->result = (int)(1000 * cos(((double)*args) / 1000));
-			// Все сделали и выходим из свича
 			break;
 		}
-		case 9: // Тангенс Изменяется быстро поэтому ввод просим умножить на 1000 а в вычислениях введенное будем делить на 1000 и выводить будем умножив на 1000, потому что быстро меняется в малых пределах
-		{
+		case 9: {
 			rez->result = (int)(1000 * tan(((double)*args) / 1000));
-			// Все сделали и выходим из свича
 			break;
 		}
-		case 10: // АркСинус Изменяется быстро поэтому ввод просим умножить на 1000 а в вычислениях введенное будем делить на 1000 и выводить будем умножив на 1000, потому что быстро меняется в малых пределах
-		{
-			//rez->result = (int)(1000 * asin(((double)*args) / 1000));
-			if ((*args >= -1000) && (*args <= 1000)) {
-				rez->result = (int)(1000 * asin(((double)*args) / 1000));
+		case 10: {
+			if ((*args < -1000) && (*args > 1000)) {
+				throw_exception = true;
 			}
-			else {
-				throw;
-			} // Конец if
-			// Все сделали и выходим из свича
+			rez->result = (int)(1000 * asin(((double)*args) / 1000));
 			break;
 		}
-		case 11: // АркКосинус Изменяется быстро поэтому ввод просим умножить на 1000 а в вычислениях введенное будем делить на 1000 и выводить будем умножив на 1000, потому что быстро меняется в малых пределах
-		{
-			//rez->result = (int)(1000 * acos(((double)*args) / 1000));
-			if ((*args >= -1000) && (*args <= 1000)) {
-				rez->result = (int)(1000 * acos(((double)*args) / 1000));
+		case 11: {
+			if ((*args < -1000) && (*args > 1000)) {
+				throw_exception = true;
 			}
-			else {
-				throw;
-			} // Конец if
-			// Все сделали и выходим из свича
+			rez->result = (int)(1000 * acos(((double)*args) / 1000));
 			break;
 		}
-		case 12: // АркТангенс Изменяется быстро поэтому ввод просим умножить на 1000 а в вычислениях введенное будем делить на 1000 и выводить будем умножив на 1000, потому что быстро меняется в малых пределах
-		{
+		case 12: {
 			rez->result = (int)(1000 * atan(((double)*args) / 1000));
-			// Все сделали и выходим из свича
 			break;
 		}
-		case 13: // Экспонента Растет быстро поэтому ввод просим умножить на 1000 а в вычислениях введенное будем делить на 1000
-		{
+		case 13: {
 			rez->result = (int)exp(((double)*args) / 1000);
-			// Все сделали и выходим из свича
 			break;
 		}
-		case 14: // Логарифм Натуральный Растет медленно поэтому вывод умножим на 1000
-		{
-			//rez->result = (int)(1000 * log((double)*args));
-			if (*args > 0) {
-				rez->result = (int)(1000 * log((double)*args));
+		case 14: {
+			if (*args <= 0) {
+				throw_exception = true;
 			}
-			else {
-				throw;
-			} // Конец if
-			// Все сделали и выходим из свича
+			rez->result = (int)(1000 * log((double)*args));
 			break;
 		}
-		case 15: // Десятичный логарифм Растет медленно поэтому вывод умножим на 1000
-		{
-			//rez->result = (int)(1000 * log10((double)*args));
-			if (*args > 0) {
-				rez->result = (int)(1000 * log10((double)*args));
+		case 15: {
+			if (*args <= 0) {
+				throw_exception = true;
 			}
-			else {
-				throw;
-			} // Конец if
-			// Все сделали и выходим из свича
+			rez->result = (int)(1000 * log10((double)*args));
 			break;
 		}
-		}; // Конец switch
+    }
 
-	}// Конец Try
-	catch (...){
-		rez->type = 0;
-	}; // Конец Catch
-
-	return rez;
+    if (throw_exception) {
+        rez->type = 0;
+    }
+    return rez;
 };
 
-__declspec(dllexport) FunctionModule* getFunctionModuleObject() {  // Нужная нам функция, вызвав ее в программе мы должны получить доступ к математическим функциям. Видимо определить их имена и т.д.
-		return new MathFunctionModule();
+__declspec(dllexport) FunctionModule* getFunctionModuleObject() {
+    return new MathFunctionModule();
 };
--- math_function_module.h
+++ math_function_module.h
@@ -8,30 +8,15 @@
 #ifndef MATH_FUNCTION_MODULE_H
 #define	MATH_FUNCTION_MODULE_H
 
-// Опишем  производный класс от FunctionModule
-class MathFunctionModule :public FunctionModule {
-public:
-	// Опишем сначала свойства класса, чтобы потом к ним обращаться
-	FunctionData **math_functions; // Это будет указатель на массив из структур куда запишем данные о математических функциях 
-	// Мы создадим заранее свойство чтобы записывать в него результат вычислений мат. функци, Это для executeFunctions
-	FunctionResult* rez;
-
-	// Метод UID 
-	const char *getUID();
-
-	// Получение функций
-	FunctionData **getFunctions(int *count_functions);
-
-	// Объявляем функцию возвращающую результат вычислений
-	FunctionResult* executeFunction(regval functionId, regval *args);
-
-	// Конструктор класса
-	MathFunctionModule();
-
-	// Деструктор напишем отдельным методом. Он удалит структуры с нашими функциями и потом сам оъект.
-	void destroy();
-
-	~MathFunctionModule() {};// не описываем деструктор но объявим его, так как он есть в базовом классе FunctionModule
+class MathFunctionModule : public FunctionModule {
+	public:
+		FunctionData **math_functions;
+		const char *getUID();
+		FunctionData **getFunctions(int *count_functions);
+		FunctionResult* executeFunction(regval functionId, regval *args);
+		MathFunctionModule();
+		void destroy();
+		~MathFunctionModule() {};
 };
 
 #endif	/* MATH_FUNCTION_MODULE_H  */
